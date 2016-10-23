06d8729620e4d3243011bb0eb2b062bd1432ad42
Michael ATem <atemhacking@gmail.com>
2016-10-23 08:27:02 +0700

    Add google plush result parse

--- collector/SNSE/ResultParser/ResultParser.py
+++ collector/SNSE/ResultParser/ResultParser.py
@@ -8,8 +8,8 @@ class Parser:
 	def __init__(self,result,domain,counter):
 		self.result 	= result
 		self.domain 	= domain
-		self.users  	= []
 		self.counter    = counter
+		self.users  	= []
 
 	def genericClean(self):
 		self.result = re.sub('<b>','',self.result)
@@ -65,13 +65,25 @@ class Parser:
 		except KeyboardInterrupt:
 			sys.exit(cprint('[-] Canceled by user','red'))
 
-	def __Email_parser(self):
+	def __Gplus_parser(self):
 		try:
-			self.genericClean()
-			e1         = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9._%+-]+' + self.domain,self.result)
-			e2         = re.findall(r'[a-zA-Z0-9._%+-]+@'+self.domain,self.result)
-			e3         = re.findall(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)' + self.domain,self.result)
-			e          = (e1+e2+e3)
-			self.counter._count_it(e)
+			self.result = re.sub('</b>', '', self.result)
+			self.result = re.sub('<b>', '', self.result)
+
+			gplus_reg   = re.compile('>[a-zA-Z0-9._ ]* - Google\+')
+			parse_it    = gplus_reg.findall(self.result)
+			count 		= self.counter._count_it(parse_it)
+			users		= self.counter._return_list()
+
+			for u in users:
+				y = string.replace(u, ' | LinkedIn','')
+				y = string.replace(y, ' profiles ','')
+				y = string.replace(y, 'LinkedIn','')
+				y = string.replace(y, '"','')
+				y = string.replace(y, '>', '')
+				if y != " ":
+					self.users.append(y)
+			return self.users
 		except KeyboardInterrupt:
 			sys.exit(cprint('[-] Canceled by user','red'))
+
