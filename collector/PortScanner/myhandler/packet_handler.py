329e1468564b96c233e628e67b2988fa0c721ccc
Michael ATem <atemhacking@gmail.com>
2016-12-11 07:57:16 +0700

    Update packet_handler.py

--- collector/PortScanner/myhandler/packet_handler.py
+++ collector/PortScanner/myhandler/packet_handler.py
@@ -99,14 +99,14 @@ class UnpackPacket:
                     return (port,serv,self.state)
 
     def _unpackAckScan(self):
-      packet       = packet[0]
+      packet       = self.packet[0]
       eth_length   = 14
       eth_header   = packet[:eth_length]
       eth          = unpack('!6s6sH' , eth_header)
       eth_protocol = socket.ntohs(eth[2])
 
       if eth_protocol == 8 :
-        ip_header   = packet[eth_length:20+eth_length]
+        ip_header   = self.packet[eth_length:20+eth_length]
         iph         = unpack('!BBHHHBBH4s4s' , ip_header)
         version_ihl = iph[0]
         version     = version_ihl >> 4
@@ -119,7 +119,7 @@ class UnpackPacket:
 
         if protocol == 6 :
           t               = iph_length + eth_length
-          tcp_header      = packet[t:t+20]
+          tcp_header      = self.packet[t:t+20]
           tcph            = unpack('!HHLLBBHHH' , tcp_header)   
           source_port     = tcph[0]
           dest_port       = tcph[1]
@@ -135,7 +135,7 @@ class UnpackPacket:
         elif protocol == 1 :
           u            = iph_length + eth_length
           icmph_length = 4
-          icmp_header  = packet[u:u+4]
+          icmp_header  = self.packet[u:u+4]
           icmph        = unpack('!BBH' , icmp_header)
           icmp_type    = icmph[0]
           code         = icmph[1]
