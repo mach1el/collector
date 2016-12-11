01e88ab5943996f27df2bf5335599117906004d9
Michael ATem <atemhacking@gmail.com>
2016-12-11 08:53:22 +0700

    Update TCG_FS.py

--- collector/TCG_FS.py
+++ collector/TCG_FS.py
@@ -16,27 +16,12 @@ from Queue import Queue
 from pprint import pprint
 from datetime import datetime
 from time import gmtime,strftime
+from MInstaller.Handler import Handler
 from argparse import ArgumentParser,RawTextHelpFormatter
+sys.dont_write_bytecode=True
 
-OS=platform.platform()
-os_name=os.name
-
-
-if os_name == 'posix':
-    c = os.system('which pip')
-    if c == 256:
-        if 'Ubuntu' or 'Lubuntu' or 'Kubuntu' or 'Xubuntu' or 'Mint' or 'Debian' or 'Ubuntu-Mate' or 'Lxle' or 'Linux-Lite' or 'Tails' or 'Kali-Linux' or 'Peppermint' or 'Bodhi-Linux' or 'Netrunner' or 'Cub-Linux' or 'Ubuntu-Gnome' or 'Ubuntu-Studio' or 'Backbox' in OS:
-            os.system('sudo apt-get install python-pip -y')
-        elif 'Fedora' or 'Redhat' or 'CentOS' in OS:
-            os.system('sudo yum install python-pip -y')
-        elif 'Arch' or 'Manrajo' in OS:
-            os.system('sudo pacman -S python-pip -y')
-        else:
-            print '[-] Try to download and install pip'
-    else:
-        pass
-else:
-    pass
+minstaller = Handler()
+minstaller.Install_Pip()
     
 try:
     import requests
@@ -49,34 +34,14 @@ try:
     from prettytable import PrettyTable
     from termcolor import colored,cprint
 except:
-    if os_name == 'posix':
-        try:
-            os.system('sudo pip install requests colorama termcolor bs4 dnspython lxml prettytable tabulate')
-        except Exception,e:
-            sys.exit(cprint(e,'red'))
-            
-    elif os_name == 'nt':
-        try:
-            os.system('c:\python27\scripts\pip.exe install requests colorama termcolor bs4 dnspython lxml prettytable tabulate')
-        except Exception,e:
-            sys.exit(cprint(e,'red'))
-    else:
-        sys.exit('[!] Try to download modules which was required')
+   minstaller.Install_modules()
 
 try:
     import pcap
     import PyQt4
 except:
-    if 'Ubuntu' or 'Lubuntu' or 'Kubuntu' or 'Xubuntu' or 'Mint' or 'Debian' or 'Ubuntu-Mate' or 'Lxle' or 'Linux-Lite' or 'Tails' or 'Kali-Linux' or 'Peppermint' or 'Bodhi-Linux' or 'Netrunner' or 'Cub-Linux' or 'Ubuntu-Gnome' or 'Ubuntu-Studio' or 'Backbox' in OS:
-        os.system('sudo apt-get install python-dev python-libpcap python-qt4 -y')
-        sys.exit()
-    elif 'Fedora' or 'Redhat' or 'CentOS' in OS:
-        os.system('sudo yum install python-libpcap python-qt4 -y')
-        sys.exit()
-    else:
-        sys.exit('[!] Try to download and install pcap module')
-        sys.exit()
-
+    minstaller.Install_pcap_pyqt()
+    sys.exit(0)
 try:
     from pWhois import Pw
     from Pinger import IPPinger
@@ -91,4 +56,4 @@ try:
     from PortScanner import xmascan,ackscan,finscan,udpscan,connscan,nullscan,stealthscan
 except ImportError,e:
     raise ImportError(e)
-sys.dont_write_bytecode=True
+
