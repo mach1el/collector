91d98f1b66c72e6cf662b9a3450da46818da3d64
Michael ATem <atemhacking@gmail.com>
2017-02-16 16:35:45 +0700

    Make this short

--- collector/TCG_FS.py
+++ collector/TCG_FS.py
@@ -1,24 +1,15 @@
+# -*- coding:utf-8 -*-
 import re
 import os
 import sys
 import time
-import json
-import string
-import struct
-import select
-import httplib
-import operator
 import platform
 from random import *
 from socket import *
-from threading import *
-from Queue import Queue
-from pprint import pprint
 from datetime import datetime
 from time import gmtime,strftime
 from MInstaller.Handler import Handler
 from argparse import ArgumentParser,RawTextHelpFormatter
-
 
 minstaller = Handler()
 minstaller.Install_Pip()
@@ -42,18 +33,18 @@ try:
 except:
     minstaller.Install_pcap_pyqt()
 try:
-    from pWhois import Pw
     from Pinger import IPPinger
     from HandlerException import *
+    from pyWhois.pyWhois import Pw
     from DNSLookup import lookup_dns
     from Traceroute import Traceroute
     from EmailHunter import EmailHunter
     from SNSE import Tsearch,Lsearch,Gplus
     from SubScan import SubScan,SubWordList
-    from WebCrawler.LinksExtractor import Extractor
-    from WebCrawler.LinksExtractor import LinksHandler
-    from WebCrawler.LinksExtractor import ExtractMultiLinks
+    from pyWhois.SubDomainHandler import DomainHandler
+    from pyWhois.RandomServer import Random_Whois_Server
     from WebCrawler.Tester.MethodsTest import MethodsTester
+    from WebCrawler.LinksExtractor import Extractor,LinksHandler,ExtractMultiLinks
     from PortScanner import xmascan,ackscan,finscan,udpscan,connscan,nullscan,stealthscan
 except ImportError,e:
     raise ImportError(e)
